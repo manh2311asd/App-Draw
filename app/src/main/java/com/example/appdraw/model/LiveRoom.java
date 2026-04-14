20bdb6d476c0ae1f20e5eb5ed9e53edaa975baeb
manh2311asd <manh2311asd@gmail.com>
2026-04-15 01:44:51 +0700

    update

---
+++ app/src/main/java/com/example/appdraw/model/LiveRoom.java
@@ -0,0 +1,12 @@
+package com.example.appdraw.model;
+
+public class LiveRoom {
+    public String roomId;
+    public String hostId;
+    public String hostName;
+    public String roomName;
+
+    public LiveRoom() {
+        // Required for Firestore
+    }
+}
